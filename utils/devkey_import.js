3bdb226372d2e34dfac177588c860c38cfb3d5b7
Greg Tracy <gtracy@gmail.com>
2021-12-31 22:13:40 -0600

    refactored gtfs import code to avoid code duplication

--- utils/devkey_import.js
+++ utils/devkey_import.js
@@ -9,17 +9,11 @@
 // prompt the user for:
 //   - File to import (default to Trips.txt)
 //
-const Fs = require('fs');
-const CsvReadableStream = require('csv-reader');
-const prompt = require('prompt-sync')({sigint: true});
-
+let gtfs = require('./gtfs');
 let config = require('../config');
 
 let AWS = require('aws-sdk');
 AWS.config.update(config.getAWSConfig())
-let ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
-let dynamoClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
-
 console.log("\nAWS config :");
 console.dir(config.getAWSConfig());
 
@@ -47,97 +41,15 @@ console.dir(config.getAWSConfig());
         }
     };
     
-    console.log("\ningest an existing dev key list into DynamoDB (not required).");
-    console.log("... create the table if it doesn't already exist");
-    let aws_result = await ddb.createTable(params, async function(err, data) {
-        if (err) {
-            if( !(err.code === "ResourceInUseException") ) {
-                // totally fine if the table already exists. 
-                // otherwise, exit.
-                console.dir(err);
-                process.exit(1);
-            } else {
-                console.error("... table " + params.TableName + " already exists");
-            }
-        } else {
-            console.log("Created table. Table description JSON: ", JSON.stringify(data, null, 2));
-            console.log("... take a quick break, and give AWS a chance to create the table ...");
-            await new Promise(r => setTimeout(r, 5000));
-        }
+    // import the GTFS file into our new table
+    try{
+        await gtfs.gtfsToDynamo(AWS," ",TABLE_NAME,params);
+    } catch(err) {
+        console.log('import failed!');
+        console.log(err);
+        process.exit(-1);
+    }
+    console.log("\nall done.\n");
 
-        var input_file = prompt("Dev key file input (not required): ");
-        if( input_file ) {
-            console.log(`\n... importing devkey data from ${input_file}`);
-
-            let inputStream = Fs.createReadStream(input_file, 'utf8');
-            let csvStream = new CsvReadableStream({ skipHeader: true, asObject: true, trim: true });
-            inputStream
-                .pipe(csvStream)
-                .on('open', () => {
-                    console.log('file opened!');
-                })
-                .on('end', async function () {
-                    if (promises.length > 0) {
-                        console.log('... awaiting write to DynamoDB\n')
-                        await Promise.all(promises);
-                    }
-
-                    // flush any remnents
-                    if (write_bucket.length > 0) {
-                        console.log('... found sme remnents. saving these now.');
-                        await saveToDynamoDB(write_bucket);
-                    }
-                    console.log('No more rows!');
-                })
-                .on('error', (err) => {
-                    console.error('Failed to read the input file ' + input_file);
-                    console.dir(err);
-                })
-                .on('data', async function (row) {
-                    // the date field in the GCP file is goofy. fix it before pushing into dynamo
-                    let date_added = row.dateAdded.split(' ');
-                    row.dateAdded = date_added[0];
-
-                    // push in new table row
-                    //console.dir(row);
-                    write_bucket.push(row);
-
-                    // if it is valid, push it into our write bucket
-                    if (write_bucket.length % batch_size === 0) {
-                        csvStream.pause();
-                        console.log(`    batch ${batch_num}`)
-
-                        promises.push(saveToDynamoDB(write_bucket));
-                        if (promises.length % concurrentRequests === 0) {
-                            console.log('... awaiting write requests to DynamoDB\n');
-                            await Promise.all(promises);
-                            promises = [];
-                        }
-
-                        write_bucket = [];
-                        batch_num++;
-                        csvStream.resume();
-                    }
-                });
-                
-
-            async function saveToDynamoDB(batch) {
-                const putReqs = batch.map(item => ({
-                    PutRequest: {
-                    Item: item
-                    }
-                }))
-                
-                const req = {
-                    RequestItems: {
-                    [TABLE_NAME]: putReqs
-                    }
-                }
-                
-                await dynamoClient.batchWrite(req).promise();
-                console.log('  batch of ' + batch.length + ' written to dynamo');
-            }
-        }
-    });
 
 })();
---
+++ utils/gtfs.js
@@ -0,0 +1,109 @@
+'use strict';
+
+//
+// core logic for importing and transforming a GTFS dataset, and
+// storing the results in a Dynamo table
+//
+// inputs
+//  AWS configuration object
+//  Dynamo table name
+
+const Fs = require('fs');
+const CsvReadableStream = require('csv-reader');
+const prompt = require('prompt-sync')({sigint: true});
+
+module.exports.gtfsToDynamo = async (AWS,gtfs_file,dynamo_table,table_params) => {
+    let ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+    let dynamoClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+        
+    const batch_size = 25;
+    let batch_num = 1;
+    let write_bucket = [];
+    let promises = [];
+    const concurrentRequests = 25;
+
+        
+    console.log("\ningest the GTFS Trips file into DynamoDB.");
+    console.log("... create the table if it doesn't already exist");
+    let aws_result = await ddb.createTable(table_params, async function(err, data) {
+        if (err) {
+            if( !(err.code === "ResourceInUseException") ) {
+                // totally fine if the table already exists. 
+                // otherwise, exit.
+                console.dir(err);
+                process.exit(1);
+            } else {
+                console.error("... table " + dynamo_table + " already exists");
+            }
+        } else {
+            console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+            console.log("... take a quick break, and give AWS a chance to create the table ...");
+            await new Promise(r => setTimeout(r, 5000));
+        }
+
+        var input_file = prompt("TFS file input ["+gtfs_file+"] : ",gtfs_file);
+        console.log(`\n... reading GTFS data from ${input_file}`);
+        console.log("... this task of writing to Dynamo may take a little while!");
+
+        let inputStream = Fs.createReadStream(input_file, 'utf8');
+        let csvStream = new CsvReadableStream({ skipHeader: true, asObject: true, trim: true });
+        inputStream
+            .pipe(csvStream)
+            .on('end', async function () {
+                if (promises.length > 0) {
+                    console.log('... awaiting write to DynamoDB\n')
+                    await Promise.all(promises);
+                }
+
+                // flush any remnents
+                if (write_bucket.length > 0) {
+                    console.log('... found sme remnents. saving these now.');
+                    await saveToDynamoDB(write_bucket);
+                }
+                console.log('No more rows!');
+            })
+            .on('error', (err) => {
+                console.error('Failed to read the input file ' + input_file);
+                console.dir(err);
+            })
+            .on('data', async function (row) {
+                //console.log('A row arrived: ');
+                write_bucket.push(row);
+
+                // if it is valid, push it into our write bucket
+                if (write_bucket.length % batch_size === 0) {
+                    console.log(`    batch ${batch_num}`)
+                    csvStream.pause();
+                    promises.push(saveToDynamoDB(write_bucket));
+                    if (promises.length % concurrentRequests === 0) {
+                        console.log('... awaiting write requests to DynamoDB\n');
+                        await Promise.all(promises);
+                        promises = [];
+                    }
+                
+                    write_bucket = [];
+                    batch_num++;
+                    csvStream.resume();
+                }
+            });
+    });
+  
+
+    async function saveToDynamoDB(batch) {
+        const putReqs = batch.map(item => ({
+            PutRequest: {
+            Item: item
+            }
+        }))
+        
+        const req = {
+            RequestItems: {
+            [dynamo_table]: putReqs
+            }
+        }
+        
+        await dynamoClient.batchWrite(req).promise();
+        console.log('  batch of ' + batch.length + ' written to dynamo');
+    }
+
+}
