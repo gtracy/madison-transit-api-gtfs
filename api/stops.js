1d55b45fcb08bed485664803c37a86163200a246
Greg Tracy <gtracy@gmail.com>
2023-05-13 10:36:50 -0500

    Cleaned up use of and labeling of stop IDs.
    The stop APIs need to work for both IDs and stop codes. These changes clean all of that up.

--- api/stops.js
+++ api/stops.js
@@ -3,7 +3,7 @@
 const moment = require('moment-timezone');
 const devkey = require('./devkey');
 const utils = require('./utils');
-const Stops = require('../lib/stops');
+const gtfs_stop = require('../lib/stops');
 
 const config = require('../config');
 const logger = require('pino')(config.getLogConfig());
@@ -15,16 +15,20 @@ module.exports = async function(app) {
     //     "status" : "0",
     //     "timestamp" : "12:38pm",
     //     "stopID" : "1391",
+    //     "stop_code" : "1391",
     //     "intersection" : "Atwood & Ohio",
     //     "latitude" : "43.0937715",
     //     "longitude" : "-89.3467281",
     // }
-    app.get('/v1/getstoplocation', utils.validateRequest, utils.afterHours, devkey.validateDevKey, async (req,res) => {
+    app.get('/v1/getstoplocation', utils.validateRequest, devkey.validateDevKey, async (req,res) => {
         var json_result = {};
-        var gtfs_stop = new Stops();
+
+        function removeLeadingZeros(str) {
+            return str.replace(/^0+/, '');
+        }          
 
         // snag the API query details
-        const stop_id = req.query.stopID;
+        const stop_id = removeLeadingZeros(req.query.stopID);
 
         json_result.status = "0";
         json_result.timestamp = moment().tz("America/Chicago").format("h:mmA");
@@ -34,6 +38,7 @@ module.exports = async function(app) {
         const stop_data = await gtfs_stop.fetchById(stop_id);
         if( stop_data ) {
             // inspect results and build the payload
+            json_result.stop_code = stop_data.stop_code;
             json_result.intersection = stop_data.stop_name;
             json_result.latitude = parseFloat(stop_data.stop_lat);
             json_result.longitude = parseFloat(stop_data.stop_lon);
